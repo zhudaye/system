9e429a081461f8f6310c092e3cddf444fab867ac
zhudaye <1607291079@qq.com>
2017-12-28 21:50:44 +0800

    ceshi

---
+++ src/components/Login.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="login">
+</div>
+</template>
+<script>
+export default {
+  name: 'Login',
+  data() {
+  	return {
+  		
+  	}
+  }
+}
+</script>
+<style scoped>
+</style>
---
+++ src/main.js
@@ -0,0 +1,24 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import iView from 'iview'
+import $ from 'jquery'
+Vue.use(iView)
+router.beforeEach((to, from, next) => {
+    iView.LoadingBar.start();
+    next();
+});
+
+router.afterEach(route => {
+    iView.LoadingBar.finish();
+});
+Vue.config.productionTip = false
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
