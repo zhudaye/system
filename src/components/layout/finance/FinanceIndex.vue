ba4f3bf7f09161246a6b87fd8d49340cf6ea68db
inshuang <1300311562@qq.com>
2018-01-09 09:49:27 +0800

    qinshuang更新

--- src/components/layout/finance/FinanceIndex.vue
+++ src/components/layout/finance/FinanceIndex.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-  	<div>FinanceIndex</div>
+  
     <router-view/>
   </div>
 </template>
---
+++ src/components/layout/finance/clientintegral/Clientintegral.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+  	<div>{{name}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'clientintegral',
+  data() {
+  	return {
+  		name: 'clientintegral'
+  	}
+  }
+}
+</script>
