7769657e568c4e7e47edf666b77b12fcd2759d74
zhudaye <1607291079@qq.com>
2018-01-09 17:18:54 +0800

    合并login

--- src/components/layout/order/orderpublic/ShowBigImg.vue
+++ src/components/layout/order/orderpublic/ShowBigImg.vue
@@ -1,6 +1,6 @@
 <template>
-	<div class="showBigImg">
-		<div class="imgbox" v-if="imginfo && imginfo.type == 'clothe'" :style="{left: left + 'px'}">
+	<div class="showBigImg" :style="{left: left + 200 + 'px'}" v-show="imginfo">
+		<div class="imgbox" v-if="imginfo && imginfo.type == 'clothes'">
 			<img :src="imginfo.bigImg">
 			<img :src="imginfo.smallImg" class="smallimg">
 			<img class="xuxian" src="static/images/ceshi/xuxian.png">
@@ -10,25 +10,19 @@
 <script>
 	export default {
 		name: 'showBigImg',
-		props:['imginfo'],
+		props:['imginfo','left'],
 		data() {
 			return {
-        left: 0
-			}
-		},
-		watch: {
-			imginfo(){
-        this.left = imginfo.left
 			}
 		}
 	}
 </script>
 <style scoped>
 .showBigImg{
-	display: none;
 	position: fixed;
 	left: 0;
 	top: 50%;
+	z-index: 9999;
 	-webkit-transform: translate(0, -50%);
 	-moz-transform: translate(0, -50%);
 	-ms-transform: translate(0, -50%);
@@ -44,7 +38,7 @@
 	display: block;
 }
 
-.smallimg.xuxian{
+.smallimg,.xuxian{
 	position: absolute;
 	left: 50%;
 	top: 20%;
@@ -53,5 +47,13 @@
 	-ms-transform: translate(-50%, 0);
 	-o-transform: translate(-50%, 0);
 	transform: translate(-50%, 0);
+}
+
+.smallimg{
+	width: 44.5%;
+}
+
+.xuxian{
+	top: 0;
 }
 </style>
